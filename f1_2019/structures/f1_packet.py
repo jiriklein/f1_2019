d6ff29133da4072946663302d4df8b3ee7a4e613
Jiri Klein <jiri.klein@quantumblack.com>
2019-11-12 13:53:07 +0000

    added inheritance logic

--- f1_2019/structures/f1_packet.py
+++ f1_2019/structures/f1_packet.py
@@ -1,12 +1,22 @@
+PACKET_HEADER = {}
+
+
 class F1Packet:
     HEADER = PACKET_HEADER
 
     def __init__(self, packet_type: int, buf: bytes):
         self._packet_type = packet_type
         if hasattr(self.__class__, "STRUCTURE"):
-            self.data = self.STRUCTURE.read_dict(buf)
+            self.data = self.STRUCTURE  # here we work with named tuples
         else:
             self.data = {}
+
+    @staticmethod
+    def read_from(buf: bytes):
+        header = F1Packet.HEADER  # here we work with named tuples
+        packet_type = header["packet_id"]  # & 0x3
+        packet_class = PACKET_TYPES.get(packet_type, F1Packet)
+        return packet_class(packet_type, buf)
 
 
 class PacketMotionData(F1Packet):
